93dd4b7baa0ce60c2679b5cae98940c946806b39
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-12-22 14:55:08 +0100

    mobileNavigation.js - 1

--- JS/script.js
+++ JS/script.js
@@ -8,10 +8,9 @@ yearEl.textContent = currentYear;
 //////////////////////////////////////
 //Make Mobile Navigation Work
 
-const btnNavEl = document.querySelector(".btn-mobile.nav");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
     headerEl.classList.toggle("nav-open");
 });
-("nav-open")
