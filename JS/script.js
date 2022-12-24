54183eabe7ac88d0d49249ee1a34ec38b08d556d
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2022-12-24 18:36:15 +0100

    stickyNavigation - finale

--- JS/script.js
+++ JS/script.js
@@ -53,11 +53,21 @@ const obs = new IntersectionObserver(
     function (entries) {
         const ent = entries[0];
         console.log(ent);
+
+        // if (ent.isIntersecting === false)
+        if (!ent.isIntersecting) {
+            document.body.classList.add("sticky");
+        }
+
+        if (ent.isIntersecting) {
+            document.body.classList.remove("sticky");
+        }
     }, 
     {
         // In the viewport
         root: null,
         threshold: 0,
+        rootMargin: "-80px",
     }
 );
 obs.observe(sectionHeroEl);
